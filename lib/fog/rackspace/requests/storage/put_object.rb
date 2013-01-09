3b9320dcde161568c90dc7d2cfb0fb52e337c6b7
Omar Qureshi <omar@omarqureshi.net>
2013-01-09 15:29:02 +0100

    Add a really simple mock for cloud files

--- lib/fog/rackspace/requests/storage/put_object.rb
+++ lib/fog/rackspace/requests/storage/put_object.rb
@@ -1,6 +1,32 @@
 module Fog
   module Storage
     class Rackspace
+
+      class Mock
+        def put_object(container, object, data, options = {})
+          data = Fog::Storage.parse_data(data)
+          data[:body] = data[:body].read unless data[:body].is_a?(String)
+          response = Excon::Response.new
+          response.status = 201
+          object = {
+            :body             => data[:body],
+            'Content-Type'    => options['Content-Type'] || data[:headers]['Content-Type'],
+            'ETag'            => Digest::MD5.hexdigest(data[:body]),
+            'Last-Modified'   => Fog::Time.now.to_date_header,
+            'Content-Length'  => options['Content-Length'] || data[:headers]['Content-Length'],
+          }
+
+          response.headers = {
+            'Content-Length'   => object['Content-Length'],
+            'Content-Type'     => object['Content-Type'],
+            'ETag'             => object['ETag'],
+            'Last-Modified'    => object['Last-Modified'],
+          }
+          response
+        end
+      end
+
+
       class Real
 
         # Create a new object
